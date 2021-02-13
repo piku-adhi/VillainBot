e0b7dda7f8be8d386e48eb470a7e19fa7418e1d1
Adithyan-kollam <73607487+piku-adhi@users.noreply.github.com>
2021-02-13 16:49:36 +0530

    Update __main__.py

--- lucifer/__main__.py
+++ lucifer/__main__.py
@@ -130,7 +130,7 @@ def start(bot: Bot, update: Update, args: List[str]):
         else:
             send_start(bot, update)
     else:
-        update.effective_message.reply_text("Yo, whadup? 🙂")
+        update.effective_message.reply_text("Yo, whatasp? 🙂")
 
 def send_start(bot, update):
     #Try to remove old message
